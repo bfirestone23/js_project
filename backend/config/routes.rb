1c181a681cfc36a18e7be29a1a5e9a4b11b4f89e
Brian Firestone <bfirestone2339@gmail.com>
2021-04-05 17:06:08 -0400

    Undo nested routing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :concerts, except: [:edit, :update] do
-    resources :comments, except: [:edit, :update]
-  end
+  resources :concerts, except: [:edit, :update]
+  resources :comments, except: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
