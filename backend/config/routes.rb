c186bdef2de5e0269e644c607eb120e2d195f36f
Brian Firestone <bfirestone2339@gmail.com>
2021-04-06 11:08:40 -0400

    Un-nest comments index

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :concerts, except: [:edit, :update] do
-    resources :comments, except: [:edit, :update]
+    resources :comments, except: [:edit, :update, :index]
   end
+
+  resources :comments, only: [:index]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
