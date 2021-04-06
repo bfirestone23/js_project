39390b1a13c5db43b426477ba7861954d349d991
Brian Firestone <bfirestone2339@gmail.com>
2021-04-06 09:49:03 -0400

    Implement nested routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :concerts, except: [:edit, :update]
-  resources :comments, except: [:edit, :update]
+  resources :concerts, except: [:edit, :update] do
+    resources :comments, except: [:edit, :update]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
