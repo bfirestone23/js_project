20a30db8411dd524d1dba3f6acfea5e49775d107
Brian Firestone <bfirestone2339@gmail.com>
2021-04-06 11:11:13 -0400

    Implement renderComment() and add concert_id attribute

--- frontend/javascript/src/comment.js
+++ frontend/javascript/src/comment.js
@@ -1,10 +1,20 @@
 class Comment {
-    constructor(content, user) {
+    constructor(content, user, concert_id) {
         this.content = content;
         this.user = user;
+        this.concert_id = concert_id;
     }
 
     // how/when to associate comment with concert?
 
     // instance method to render comment object to the DOM
+    renderComment() {
+        let commentsDiv = document.querySelector(`div#comments-${this.concert_id}`)
+        commentsDiv.innerHTML +=
+        `<div>
+            <h6>${this.user}</h6>
+            <p>${this.content}</p>
+        </div>
+        `
+    }
 }
