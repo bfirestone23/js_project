869204d6274d4fe8334aaef57280cce348787efa
Brian Firestone <bfirestone2339@gmail.com>
2021-04-06 13:09:14 -0400

    Remove comment and adjust spacing

--- frontend/javascript/src/comment.js
+++ frontend/javascript/src/comment.js
@@ -5,13 +5,12 @@ class Comment {
         this.concert_id = concert_id;
     }
 
-    // how/when to associate comment with concert?
-
     // instance method to render comment object to the DOM
     renderComment() {
         let commentsDiv = document.querySelector(`div#comments-${this.concert_id}`)
         commentsDiv.innerHTML +=
-        `<div>
+        `
+        <div>
             <p class="comment-user">Comment by ${this.user}:</p>
             <p class="comment-content">${this.content}</p>
         </div>
