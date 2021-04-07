46643cfe5b5eeca752e7f6e82b80deafe5271f05
Brian Firestone <bfirestone2339@gmail.com>
2021-04-07 16:06:17 -0400

    Move renderConcert above previous method, specify concert ID in comment form inputs

--- frontend/javascript/src/concert.js
+++ frontend/javascript/src/concert.js
@@ -11,30 +11,6 @@ class Concert {
         this.user = user;
     }
 
-    renderConcertCommentSection = () => {
-        let concertDiv = document.getElementById(`concert-${this.id}-container`)
-        concertDiv.innerHTML +=
-        `
-        <div class="container-fluid">
-            <div id="comments-${this.id}" class="comments-container">
-            </div>
-            <form class="comment-form" data-id="${this.id}" id="comment-form-${this.id}">
-                <div class="form-group">
-                    <label class="sr-only" for="comment-user">Leave comment as:</label>
-                    <input placeholder="Username" id="form-comment-user" class="comment-user form-control" type="text">
-                </div>
-
-                <div class="form-group">
-                    <label class="sr-only" for="comment-content">Your comment:</label>
-                    <textarea placeholder="Comment" id="form-comment-content" class="comment-content form-control"></textarea>
-                </div>
-                <input class="comment-submit btn btn-dark" type="submit" value="Submit Comment">
-            </form>
-        </div>
-        `
-        addCommentFormListener(this.id);
-    }
-
     // renders concert object to the DOM
     renderConcert = () => {
         let concertsDiv = document.getElementById('concerts-container')
@@ -46,19 +22,45 @@ class Concert {
                     <h3>${this.artist} at ${this.venue}</h3>
                     <h4>${this.date}</h4>
                     <h5>Posted by ${this.user}</h5>
+                    <p><strong>Attended by</strong> ${this.attendees}</p>
                 </div>
                 <div class="concert-info">
-                    <p class="concert-info-title">Attended by ${this.attendees}</p>
-                    <p class="concert-info-title">Highlights: ${this.highlights}</p>
-                    <p class="concert-info-title">Lowlights: ${this.lowlights}</p>
+                    <h5><strong>Highlights:</strong></h5> 
+                    <p>${this.highlights}</p>
+                    <h5><strong>Lowlights:</strong></h5>
+                    <p>${this.lowlights}</p>
                 </div>
-                <button class="delete-btn btn btn-dark" onClick="deleteConcert()" data-id="${this.id}">Delete Concert</button>
+                <button class="hidden delete-btn btn btn-dark" onClick="deleteConcert()" data-id="${this.id}">Delete Concert</button>
             </div>
             <hr>
         </div>
         `
         this.renderConcertCommentSection();
         
+    }
+
+    renderConcertCommentSection = () => {
+        let concertDiv = document.getElementById(`concert-${this.id}-container`)
+        concertDiv.innerHTML +=
+        `
+        <div class="container-fluid">
+            <div id="comments-${this.id}" class="comments-container">
+            </div>
+            <form class="comment-form" data-id="${this.id}" id="comment-form-${this.id}">
+                <div class="form-group">
+                    <label class="sr-only" for="comment-user">Leave comment as:</label>
+                    <input placeholder="Username" id="${this.id}-comment-user" class="comment-user form-control" type="text">
+                </div>
+
+                <div class="form-group">
+                    <label class="sr-only" for="comment-content">Your comment:</label>
+                    <textarea placeholder="Comment" id="${this.id}-comment-content" class="comment-content form-control"></textarea>
+                </div>
+                <input class="comment-submit btn btn-dark" type="submit" value="Submit Comment">
+            </form>
+        </div>
+        `
+        addCommentFormListener(this.id);
     }
     
 }
