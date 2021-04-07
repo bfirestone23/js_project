f4b1551750875269034ba4e572b07c28fd901f11
Brian Firestone <bfirestone2339@gmail.com>
2021-04-07 10:30:23 -0400

    Add comments show route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :concerts, except: [:edit, :update] do
-    resources :comments, except: [:edit, :update, :index]
+    resources :comments, except: [:edit, :update]
   end
 
-  resources :comments, only: [:index]
+  resources :comments, only: [:index, :create, :show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
