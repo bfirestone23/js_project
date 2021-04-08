67fc1bac5c5c099f312affb7f746a3cef76f3c6d
Brian Firestone <bfirestone2339@gmail.com>
2021-04-07 20:43:27 -0400

    Add validations

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
     belongs_to :concert
+    validates_presence_of :user, :concert_id, :content
 end
--- backend/app/models/concert.rb
+++ backend/app/models/concert.rb
@@ -1,3 +1,8 @@
 class Concert < ApplicationRecord
     has_many :comments
+    validates_presence_of :artist, :venue, :attendees, :highlights, :lowlights, :user, :date
+
+    def date_formatted
+        self.date.strftime("%m/%d/%Y")
+    end
 end
